ec6a7c9a846d3b7c2adc3ce389029c6a1cb4d48d
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-04-27 20:24:20 +1000

    Create UILib.lua

---
+++ UILib.lua
@@ -0,0 +1,166 @@
+--[[
+
+		Gui2Lua™
+		10zOfficial
+		Version 1.0.0
+
+]]
+
+
+-- Instances
+
+local KryptoniteWareUI = Instance.new("ScreenGui")
+local BlatantWindow = Instance.new("Frame")
+local BlatantLabel = Instance.new("TextLabel")
+local UICorner = Instance.new("UICorner")
+local CumAuraButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local RenderWindow = Instance.new("Frame")
+local RenderLabel = Instance.new("TextLabel")
+local UICorner_3 = Instance.new("UICorner")
+local WaifuThighsButton = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local UtilityWindow = Instance.new("Frame")
+local UtilityLabel = Instance.new("TextLabel")
+local UICorner_5 = Instance.new("UICorner")
+local CombatWindow = Instance.new("Frame")
+local CombatLabel = Instance.new("TextLabel")
+local UICorner_6 = Instance.new("UICorner")
+local CloseButton = Instance.new("TextButton")
+
+-- Properties
+
+KryptoniteWareUI.Name = "KryptoniteWareUI"
+KryptoniteWareUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+KryptoniteWareUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+BlatantWindow.Name = "BlatantWindow"
+BlatantWindow.Parent = KryptoniteWareUI
+BlatantWindow.BackgroundColor3 = Color3.new(0.282353, 0.552941, 0.184314)
+BlatantWindow.BorderSizePixel = 4
+BlatantWindow.Position = UDim2.new(0.306504846, 0, 0.131787911, 0)
+BlatantWindow.Size = UDim2.new(0, 250, 0, 519)
+
+BlatantLabel.Name = "BlatantLabel"
+BlatantLabel.Parent = BlatantWindow
+BlatantLabel.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+BlatantLabel.Position = UDim2.new(0.0360000283, 0, 0.0154142585, 0)
+BlatantLabel.Size = UDim2.new(0, 232, 0, 37)
+BlatantLabel.Font = Enum.Font.GothamBold
+BlatantLabel.Text = "Blatant"
+BlatantLabel.TextColor3 = Color3.new(1, 1, 1)
+BlatantLabel.TextScaled = true
+BlatantLabel.TextSize = 14
+BlatantLabel.TextStrokeTransparency = 0.4000000059604645
+BlatantLabel.TextWrapped = true
+
+UICorner.Parent = BlatantLabel
+
+CumAuraButton.Name = "CumAuraButton"
+CumAuraButton.Parent = BlatantWindow
+CumAuraButton.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+CumAuraButton.Position = UDim2.new(0.0359999985, 0, 0.100192681, 0)
+CumAuraButton.Size = UDim2.new(0, 232, 0, 33)
+CumAuraButton.Font = Enum.Font.GothamBold
+CumAuraButton.Text = "CumAura"
+CumAuraButton.TextColor3 = Color3.new(1, 1, 1)
+CumAuraButton.TextScaled = true
+CumAuraButton.TextSize = 14
+CumAuraButton.TextStrokeTransparency = 0.4000000059604645
+CumAuraButton.TextWrapped = true
+
+UICorner_2.Parent = CumAuraButton
+
+RenderWindow.Name = "RenderWindow"
+RenderWindow.Parent = KryptoniteWareUI
+RenderWindow.BackgroundColor3 = Color3.new(0.282353, 0.552941, 0.184314)
+RenderWindow.BorderSizePixel = 4
+RenderWindow.Position = UDim2.new(0.507344127, 0, 0.131787926, 0)
+RenderWindow.Size = UDim2.new(0, 250, 0, 519)
+
+RenderLabel.Name = "RenderLabel"
+RenderLabel.Parent = RenderWindow
+RenderLabel.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+RenderLabel.Position = UDim2.new(0.0360000283, 0, 0.0134874759, 0)
+RenderLabel.Size = UDim2.new(0, 232, 0, 37)
+RenderLabel.Font = Enum.Font.GothamBold
+RenderLabel.Text = "Render"
+RenderLabel.TextColor3 = Color3.new(1, 1, 1)
+RenderLabel.TextScaled = true
+RenderLabel.TextSize = 14
+RenderLabel.TextStrokeTransparency = 0.4000000059604645
+RenderLabel.TextWrapped = true
+
+UICorner_3.Parent = RenderLabel
+
+WaifuThighsButton.Name = "WaifuThighsButton"
+WaifuThighsButton.Parent = RenderWindow
+WaifuThighsButton.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+WaifuThighsButton.Position = UDim2.new(0.0359999985, 0, 0.100192681, 0)
+WaifuThighsButton.Size = UDim2.new(0, 232, 0, 33)
+WaifuThighsButton.Font = Enum.Font.GothamBold
+WaifuThighsButton.Text = "WaifuThighs"
+WaifuThighsButton.TextColor3 = Color3.new(1, 1, 1)
+WaifuThighsButton.TextScaled = true
+WaifuThighsButton.TextSize = 14
+WaifuThighsButton.TextStrokeTransparency = 0.4000000059604645
+WaifuThighsButton.TextWrapped = true
+
+UICorner_4.Parent = WaifuThighsButton
+
+UtilityWindow.Name = "UtilityWindow"
+UtilityWindow.Parent = KryptoniteWareUI
+UtilityWindow.BackgroundColor3 = Color3.new(0.282353, 0.552941, 0.184314)
+UtilityWindow.BorderSizePixel = 4
+UtilityWindow.Position = UDim2.new(0.706684589, 0, 0.131787911, 0)
+UtilityWindow.Size = UDim2.new(0, 250, 0, 519)
+
+UtilityLabel.Name = "UtilityLabel"
+UtilityLabel.Parent = UtilityWindow
+UtilityLabel.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+UtilityLabel.Position = UDim2.new(0.0360000283, 0, 0.0154142585, 0)
+UtilityLabel.Size = UDim2.new(0, 232, 0, 37)
+UtilityLabel.Font = Enum.Font.GothamBold
+UtilityLabel.Text = "Utility"
+UtilityLabel.TextColor3 = Color3.new(1, 1, 1)
+UtilityLabel.TextScaled = true
+UtilityLabel.TextSize = 14
+UtilityLabel.TextStrokeTransparency = 0.4000000059604645
+UtilityLabel.TextWrapped = true
+
+UICorner_5.Parent = UtilityLabel
+
+CombatWindow.Name = "CombatWindow"
+CombatWindow.Parent = KryptoniteWareUI
+CombatWindow.BackgroundColor3 = Color3.new(0.282353, 0.552941, 0.184314)
+CombatWindow.BorderSizePixel = 4
+CombatWindow.Position = UDim2.new(0.10566546, 0, 0.131787911, 0)
+CombatWindow.Size = UDim2.new(0, 250, 0, 519)
+
+CombatLabel.Name = "CombatLabel"
+CombatLabel.Parent = CombatWindow
+CombatLabel.BackgroundColor3 = Color3.new(0.427451, 0.835294, 0.278431)
+CombatLabel.Position = UDim2.new(0.0439999998, 0, 0.0154142585, 0)
+CombatLabel.Size = UDim2.new(0, 232, 0, 37)
+CombatLabel.Font = Enum.Font.GothamBold
+CombatLabel.Text = "Combat"
+CombatLabel.TextColor3 = Color3.new(1, 1, 1)
+CombatLabel.TextScaled = true
+CombatLabel.TextSize = 14
+CombatLabel.TextStrokeTransparency = 0.4000000059604645
+CombatLabel.TextWrapped = true
+
+UICorner_6.Parent = CombatLabel
+
+CloseButton.Name = "CloseButton"
+CloseButton.Parent = KryptoniteWareUI
+CloseButton.BackgroundColor3 = Color3.new(1, 0.262745, 0.27451)
+CloseButton.BorderSizePixel = 4
+CloseButton.Position = UDim2.new(0.0607014596, 0, 0.131504521, 0)
+CloseButton.Size = UDim2.new(0, 48, 0, 45)
+CloseButton.Font = Enum.Font.GothamBold
+CloseButton.Text = "X"
+CloseButton.TextColor3 = Color3.new(0, 0, 0)
+CloseButton.TextScaled = true
+CloseButton.TextSize = 14
+CloseButton.TextWrapped = true
